f9733dcae740cc59dfcee03657f1fb95d31d4d14
abhiram10199 <68800549+abhiram10199@users.noreply.github.com>
2022-11-03 10:51:46 +0530

    Update code.py

--- code.py
+++ code.py
@@ -2,27 +2,28 @@ import os
 from pathlib import Path
 
 Legend = {
-	'Docutments':['.pdf', '.txt'. '.docx', '.csv'],
+	'Docutments':['.pdf', '.txt', '.docx', '.csv'],
 	'Pictures':['.png', '.jpg', '.jpeg', '.webp','.gif'],
 	'Music':['.mp3', '.m4a', '.m4b'],
-	'Video':['.mp4', '.vlc'. '.mkv']
+	'Video':['.mp4', '.vlc', '.mkv']
 }
 
 def WhichDir(hint):
-	for categ, extension in Legend.items():      # looks at the list of extens for each key from the dict 'Legend'
-		for fileType in extension:									
-			return categ if fileType == hint     # if the type(file) matches with any one in the list, it returns the 'destination directory'
+    for categ, extension in Legend.items():  # looks at the list of extens for each key from the dict 'Legend'
+        for fileType in extension:									
+            if fileType == hint:             # if the type(file) matches with any one in the list, it returns the 'destination directory'
+                return categ
     
 def OrganiseDirectory():
   for item in os.scandir():
-    if item.is_dir(): continue             # checks whether 'item' is a file or a folder. If latter, skips and picks up another item
-  
-  FilePath = Path(item)                    # gets the path of the file
-  FileType = filePath.suffix.lower()       # gets the type(file) from the extension
-  Directory = WhichDir(fileType)           # assigns seatination directory to variable
-  
-  if Directory == None: 	           # if file extension isn't given in the Legend, skip
-		continue
-		
-  DirectoryPath = Path(Directory)					 
-	filePath.rename(directoryPath.joinpath(filePath))    # sends the item into proper directory
+    if item.is_dir(): continue               # checks whether 'item' is a file or a folder. If latter, skips and picks up another item
+    
+    FilePath = Path(item)                    # gets the path of the file
+    FileType = FilePath.suffix.lower()       # gets the type(file) from the extension
+    Directory = WhichDir(FileType)           # assigns seatination directory to variable
+    
+    if Directory == None: 	             # if file extension isn't given in the Legend, skip
+            continue
+            
+    DirectoryPath = Path(Directory)					 
+    FilePath.rename(DirectoryPath.joinpath(FilePath))    # sends the item into proper directory
